60f2491983e827ce6a43d06c2cfc8f395afc5c28
Mihir Vora <mihir.v@simformsolutions.com>
2023-01-12 13:03:20 +0530

    mul of two number | master branch

---
+++ mul.py
@@ -0,0 +1,9 @@
+class Solution:
+	# sum of two number
+	def mul_two_number(self, a=0, b=0):
+		return int(a) * int(b)
+
+if __name__ == '__main__':
+	ob = Solution()
+	res = ob.mul_two_number(10, 20)
+	print(res)
