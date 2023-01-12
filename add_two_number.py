e84244d49ea3a9402aa434c5cf224d0ffdfbdefe
Mihir Vora <mihir.v@simformsolutions.com>
2023-01-12 13:01:33 +0530

    sum of two number function created |  master branch

---
+++ add_two_number.py
@@ -0,0 +1,9 @@
+class Solution:
+	# sum of two number
+	def add_two_number(self, a=0, b=0):
+		return int(a) + (b)
+
+if __name__ == '__main__':
+	ob = Solution()
+	res = ob.add_two_number(10, 20)
+	print(res)
