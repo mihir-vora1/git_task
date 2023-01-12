e64215a2c7d1ccc2b98683dfecaaa073aff21d2d
Mihir Vora <mihir.v@simformsolutions.com>
2023-01-12 14:21:24 +0530

    delect the loop in linked list

---
+++ detect_loop_linked_list.py
@@ -0,0 +1,56 @@
+
+# Node class
+class Node:
+
+	# Constructor to initialize
+	# the node object
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+
+class LinkedList:
+
+	# Function to initialize head
+	def __init__(self):
+		self.head = None
+
+
+	def push(self, new_data):
+		new_node = Node(new_data)
+		new_node.next = self.head
+		self.head = new_node
+
+	def printList(self):
+		temp = self.head
+		while(temp):
+			print(temp.data, end=" ")
+			temp = temp.next
+
+	def detectLoop(self):
+		s = set()
+		temp = self.head
+		while (temp):
+			if (temp in s):
+				return True
+			s.add(temp)
+
+			temp = temp.next
+
+		return False
+
+
+# Driver program for testing
+llist = LinkedList()
+llist.push(20)
+llist.push(4)
+llist.push(15)
+llist.push(10)
+
+# Create a loop for testing
+llist.head.next.next.next.next = llist.head
+
+if(llist.detectLoop()):
+	print("Loop Found")
+else:
+	print("No Loop ")
